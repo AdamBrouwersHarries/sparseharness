acbd3868e2802781695d6febb2c0c7ffbe30b5f6
Adam Harries <adam.harries@ed.ac.uk>
2017-08-14 16:07:48 +0100

    add progress to gather all

--- scripts/experiments/gather_all.sh
+++ scripts/experiments/gather_all.sh
@@ -4,7 +4,10 @@ resultfolder=$1
 files=$(find $resultfolder -name "*.txt")
 touch aggregate_result.txt
 
+count=$(echo $files | wc -l)
+i=0
 for f in $files; do
-	echo "reading data from $f"
+	echo "reading data from $f - $i/$count"
+	i=$((i++))
 	cat $f >> aggregate_result.txt
 done
