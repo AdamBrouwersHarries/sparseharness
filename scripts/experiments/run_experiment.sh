cd7a1d221bf54d3d65a9bcaa7fd1a3854c5924a3
Adam Harries <s1467120@renown.inf.ed.ac.uk>
2017-08-16 18:47:04 +0100

    add run experiment script for cdtcluster to repo

---
+++ scripts/experiments/run_experiment.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ~/spmvharness/scripts/experiments/
+./run_all.sh ~/mdatasets/original/ ../../build/spmv_harness ~/kdatasets/kernels/e9d0b5b53766f985811eb7a0e6b92b33c757b864/spmv/ ../../example/runfile2.csv 0 0 
+
