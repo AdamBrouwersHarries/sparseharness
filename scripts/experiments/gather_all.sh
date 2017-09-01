0e4d1743a1c6a4c60e6a70b1066b5fe220dc680b
Adam Harries <adam.harries@ed.ac.uk>
2017-09-01 10:18:18 +0100

    updated gather all to just gather profiling and sql data

--- scripts/experiments/gather_all.sh
+++ scripts/experiments/gather_all.sh
@@ -2,12 +2,16 @@
 resultfolder=$1
 
 find $resultfolder -name "*.txt" > files.txt
-echo "" > aggregate_result.txt
+# echo "" > aggregate_result.txt
+echo "" > aggregate_profiling_data.txt
+echo "" > aggregate_sql_data.txt
 
 count=$(cat files.txt | wc -l)
 i=0
 for f in $(cat files.txt); do
 	echo "reading data from $f - $i/$count"
 	i=$(($i + 1))
-	cat $f >> aggregate_result.txt
+	grep "PROFILING_DATUM" $f >> aggregate_profiling_data.txt
+	grep "INSERT INTO" $f >> aggregate_sql_data.txt
+	# cat $f >> aggregate_result.txt
 done
