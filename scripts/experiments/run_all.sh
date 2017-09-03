3043b2fc47260b7180d3ea5d834ab9ec87a812a1
Adam Harries <harries.adam@gmail.com>
2017-09-03 17:05:38 +0100

    add more precision to estimated days to take

--- scripts/experiments/run_all.sh
+++ scripts/experiments/run_all.sh
@@ -89,7 +89,7 @@ do
 		echo "Run took $runtime seconds, total time of $scripttime seconds"
 		echo "Run took $runtime seconds, total time of $scripttime seconds" >> runstatus.txt
 
-		estimated_total=$(bc <<< "(($scripttime/$i)*$taskcount)/(60*60*24)")
+		estimated_total=$(bc <<< "scale=3;(($scripttime/$i)*$taskcount)/(60*60*24)")
 
 		echo "Estimated total days: $estimated_total" 
 		echo "Estimated total days: $estimated_total" >> runstatus.txt
