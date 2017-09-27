aad4fed6db485ca425e53ff6ab4695ccacfbbd48
Adam Harries <adam.harries@ed.ac.uk>
2017-09-27 18:34:55 +0100

    eliminate redundncy in kernel updating scripts

--- example/updatekernels.sh
+++ example/updatekernels.sh
@@ -5,3 +5,4 @@ cp -f $d/awrg-alcl-alcl-edp-split-512.json kernel2.json
 cp -f $d/swrg-slcl-sdp-chunk-128.json kernel3.json
 cp -f $d/awrg-alcl-alcl-edp-split-8.json kernel4.json
 cp -f $d/glb-sdp.json kernel5.json
+cp -f $d/glb-sdp-rsa.json kernel6.json
