e6b4df6a06d692b88e78b0665c584f1d71c6b341
Adam Harries <adam.harries@ed.ac.uk>
2017-10-02 15:37:38 +0100

    replace kernels with ones with parameters ordered correctly

--- scripts/example/spmv.sh
+++ scripts/example/spmv.sh
@@ -5,9 +5,9 @@ projroot=$1
 # The harness executable
 harness=$projroot/build/spmv_harness
 # A kernel to run with the harness
-kernel=$projroot/example/kernel6.json
+kernel=$projroot/example/kernel5.json
 # The matrix to process
-matrix=$projroot/example/matrix3.mtx
+matrix=$projroot/example/matrix2.mtx
 # The run parameters - i.e. local and global sizes
 runfile=$projroot/example/runfile.csv
 # our hostname
