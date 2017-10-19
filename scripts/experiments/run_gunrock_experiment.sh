c4ae9a9e4344fc320a0744e28629e432a839eecd
Adam Harries <adam.harries@ed.ac.uk>
2017-10-19 02:22:11 +0100

    change platform for cc2

--- scripts/experiments/run_gunrock_experiment.sh
+++ scripts/experiments/run_gunrock_experiment.sh
@@ -8,4 +8,4 @@ popd
 cd scripts/experiments
 # ./run_all.sh ~/mdatasets/original/ ../../build/spmv_harness ~/kdatasets/kernels/e9d0b5b53766f985811eb7a0e6b92b33c757b864/spmv/ ../../example/runfile2.csv 0 0 
 
-./run_all.sh ~/scratch/mdatasets/gunrock/ ../../build/spmv_harness ~/scratch/kdatasets/kernels/a88d68e00740611dcb61dcab0f66906e8f0bbd1a/spmv/ ../../example/runfile2.csv 0 0 ~/scratch/s1467120
+./run_all.sh ~/scratch/mdatasets/gunrock/ ../../build/spmv_harness ~/scratch/kdatasets/kernels/a88d68e00740611dcb61dcab0f66906e8f0bbd1a/spmv/ ../../example/runfile2.csv 1 0 ~/scratch/s1467120
