209f1f9a8b202e57704674a0b826ec35c864335e
Adam Harries <harries.adam@gmail.com>
2017-10-23 17:11:01 +0100

    add query building script

---
+++ scripts/experiments/postprocessing/build_query.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build a query from a folder containing results
+# get the directory containing (recursively) the results
+resdir=$1
+# get the name of a table to insert into
+table=$2
+
+
+# make a temporary folder for the results
+brdir=$(basename $resdir)
+tdir="/tmp/scratchharness/$bdir"
+mkdir -p $tdir
+
+# grep the results from the folder recursively, 
+# and subsitute various parts of it
+
+grep -r "INSERT" . | sed "s/.*INSERT/INSERT/g" | sed "s/table_name/$table" > $tdir/query.sql
