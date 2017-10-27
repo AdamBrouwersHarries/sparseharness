9f43e8105b63e617156a95ef73c999e6ab195ec3
Adam Harries <harries.adam@gmail.com>
2017-10-27 23:44:34 +0100

    add kernel selection to generic script

--- scripts/experiments/run_generic_experiment.sh
+++ scripts/experiments/run_generic_experiment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dataset=$1
-platform=$2
+kernels=$2
+platform=$3
 
 cd ~/projects/sparseharness/
 mkdir -p build
@@ -11,4 +12,4 @@ popd
 cd ~/projects/sparseharness/scripts/experiments
 # ./run_all.sh ~/mdatasets/original/ ../../build/spmv_harness ~/kdatasets/kernels/e9d0b5b53766f985811eb7a0e6b92b33c757b864/spmv/ ../../example/runfile2.csv 0 0 
 
-./run_all.sh $dataset ../../build/spmv_harness ~/scratch/kdatasets/kernels/head/spmv/ ../../example/runfile2.csv $platform 0 ~/scratch/s1467120
+./run_all.sh $dataset ../../build/spmv_harness $kernels ../../example/runfile2.csv $platform 0 ~/scratch/s1467120
